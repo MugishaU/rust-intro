1ae9010d59a56a9d25e0767811751c3b36b14157
MugishaU <muwiragiye123@gmail.com>
2024-05-23 14:43:26 +0100

    try out functions with numbers

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
 fn main() {
-  const NAME: &str = "Mugisha";
-  println!("Hello, world! My name is {NAME}.");
+  fn double(num: u128) -> u128 {
+    num * 2
+  }
+
+  // let int: i32 = 32;
+  let big_int = 105;
+  // let float = 1.2;
+
+  let input = big_int;
+
+  let output = double(big_int);
+  // let doubled_outcome = outcome;
+
+  println!("{input} doubled is {output}");
 }
